3ce8a30524161574cdeaf45535a6083a7194ae13
Brad Macdonald <bwm03570@gmail.com>
2022-11-14 10:53:11 -0700

    fixes typo, adds args to docstrings

--- bin/metadata_validation.py
+++ bin/metadata_validation.py
@@ -8,10 +8,13 @@ import json
 import synapseclient
 
 
-def get_synaspe_file(syn_id: str) -> synapseclient.entity.File:
+def get_synapse_file(syn_id: str) -> synapseclient.entity.File:
     """
     1. Validates provided Synapse ID against regex pattern
     2. Initializes synapseclient session and retrieves file metadata
+
+    Args:
+        syn_id (str): Synapse ID
     """
     pattern = re.compile("syn[0-9]+")
     if not pattern.search(syn_id):
@@ -25,16 +28,26 @@ def get_synaspe_file(syn_id: str) -> synapseclient.entity.File:
 def json_dump(syn_id: str, test_name: str, data: dict):
     """
     Dumps dictionary into json file
+
+    Args:
+        syn_id (str): Synapse ID
+        test_name (str): Validation test that was performed
+        data (dict): Formatted dictionary to be dumped into output JSON file
     """
     with open(f'{syn_id}-{test_name}.json', "w") as outfile:
         json.dump(data, outfile)
 
 
-def execute_file_entity_test(syn_id: str, syn_file: str, md5_checksum: str):
+def execute_file_entity_test(syn_id: str, syn_file: synapseclient.entity.File, md5_checksum: str):
     """
     1. Creates file_entity_test dictionary for eventual json dump
     2. Checks synapse file for concreteType if FileEntity
     3. Calls json_dump to create json file with validation result and input metadata
+
+    Args:
+        syn_id (str): Synapse ID
+        syn_file (synapseclient.entity.File): Synapse file entity
+        md5_checksum (str): MD5 Checksum from input CSV file, to be carried on for Tier 1 Valiation step
     """
     # Perform the file entity test
     concrete_type = syn_file.get("concreteType", "")
@@ -62,5 +75,5 @@ def execute_file_entity_test(syn_id: str, syn_file: str, md5_checksum: str):
 if __name__ == '__main__':
     syn_id = sys.argv[1]
     md5_checksum = sys.argv[2]
-    syn_file = get_synaspe_file(syn_id)
+    syn_file = get_synapse_file(syn_id)
     execute_file_entity_test(syn_id, syn_file, md5_checksum)
